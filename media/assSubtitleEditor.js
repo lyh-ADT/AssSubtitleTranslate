fd88c57a8a7a866750d76278b21feea69e573179
lyh-ADT <1043970238@qq.com>
2020-07-02 22:54:25 +0800

    created AssSubtitle class and SubtitleLine class

--- media/assSubtitleEditor.js
+++ media/assSubtitleEditor.js
@@ -1,12 +1,40 @@
 // @ts-check
+class SubtitleLine{
+    constructor(/**@type {number}*/line_number, /**@type {string}*/info, /**@type {string}*/content){
+        this.line_number = line_number;
+        this.info = info;
+        this.content = content;
+    }
 
+    toString(){
+        return `{info:'${this.info}',content:'${this.content}'}`;
+    }
+}
+class AssSubtitle{
+    
+    constructor(/**@type {string}*/text){
+        this.lines = this.parse(text);
+    }
+
+    parse(/**@type {string}*/text){
+        let lines = [];
+        const dialogue_re = /Dialogue: \d+,\d+:\d+:\d+.\d+,\d+:\d+:\d+.\d+,\w+,,\d,\d,\d,,(.*)/g;
+        for(let line of text.match(dialogue_re)){
+            let match = line.match(/(Dialogue: \d+,\d+:\d+:\d+.\d+,\d+:\d+:\d+.\d+,\w+,,\d,\d,\d,,)(.*)/);
+            lines.push(new SubtitleLine(0, match[1], match[2]));
+        }
+        document.querySelector("h1").innerHTML = lines.join("<br>");
+        return lines;
+    }
+}
 
 (function(){
     // @ts-ignore
     const vscode = acquireVsCodeApi();
     
     function updateContent(/**@type {string}*/text){
-        document.querySelector("h1").textContent = text;
+        // document.querySelector("h1").textContent = text;
+        new AssSubtitle(text);
     }
 
     window.addEventListener("message", event=>{
